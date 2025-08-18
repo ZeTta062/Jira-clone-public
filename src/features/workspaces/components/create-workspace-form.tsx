7749b48b0621ac8ff8ce417759eef3c986316a19
ZeTta062 <samhak17@naver.com>
2025-08-18 17:48:03 +0900

    upload Image

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -1,10 +1,14 @@
 "use client"
 
 import { z } from "zod";
+import { useRef } from "react";
+import Image from "next/image";
+import { ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Input } from "@/components/ui/input";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -19,6 +23,7 @@ interface CreateWorkspaceFormPros {
 
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormPros) => {
     const { mutate, isPending } = useCreateWorkspace();
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const form = useForm<z.infer<typeof createWorkspaceSchema>>({
         resolver: zodResolver(createWorkspaceSchema),
@@ -28,8 +33,25 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormPros) => {
     });
 
     const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
-        mutate({ json: values });
+        const finalValues = {
+            ...values,
+            image: values.image instanceof File ? values.image : "",
+        };
+
+        mutate({ form: finalValues }, {
+            onSuccess: () => {
+                form.reset();
+                // Todo:  새로운 워크스페이스에 리다이렉트하기
+            }
+        });
     };
+
+    const handleImageChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            form.setValue("image", file);
+        }
+    }
 
     return (
         <Card className="w-full h-full border-none shadow-none">
@@ -48,7 +70,7 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormPros) => {
                             <FormField 
                                 control={form.control}
                                 name="name"
-                                render={({ field}) => (
+                                render={({ field }) => (
                                     <FormItem>
                                         <FormLabel>
                                             워크스페이스 이름
@@ -61,6 +83,60 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormPros) => {
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={form.control}
+                                name="image"
+                                render={({ field }) => (
+                                    <div className="flex flex-col gap-y-2">
+                                        <div className="flex items-center gap-x-5">
+                                            {field.value ? (
+                                                <div className="size-[72px] relative rounded-md overflow-hidden">
+                                                    <Image 
+                                                        src={
+                                                            field.value instanceof File
+                                                            ? URL.createObjectURL(field.value)
+                                                            : field.value
+                                                        }
+                                                        fill
+                                                        alt="로고"
+                                                        className="object-cover"
+                                                    />
+                                                </div>
+                                            ) : (
+                                                <Avatar className="size-[72px]">
+                                                    <AvatarFallback >
+                                                        <ImageIcon className="size-[36px] text-neutral-400" />
+                                                    </AvatarFallback>
+                                                </Avatar>
+                                            )}
+                                            <div className="flex flex-col">
+                                                <p className="text-sm">워크스페이스 아이콘</p>
+                                                <p className="text-sm text-muted-foreground">
+                                                    JPG, PNG, SVG 또는 JPEG | 최대 1mb
+                                                </p>
+                                                <input 
+                                                    className="hidden"
+                                                    accept=".jpg, .png, .svg, .jpeg"
+                                                    type="file"
+                                                    ref={inputRef}
+                                                    onChange={handleImageChange}
+                                                    disabled={isPending}
+                                                />
+                                                <Button
+                                                    type="button"
+                                                    variant={"tertiary"}
+                                                    size={"xs"}
+                                                    className="w-fit mt-2"
+                                                    onClick={() => inputRef.current?.click()}
+                                                    disabled={isPending}
+                                                >
+                                                    이미지 업로드
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 )}
                             />
                         </div>
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { ID } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
 
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { createWorkspaceSchema } from "../schemas";
@@ -10,13 +10,38 @@ import { createWorkspaceSchema } from "../schemas";
 const app = new Hono()
     .post(
         "/",
-        zValidator("json", createWorkspaceSchema),
+        zValidator("form", createWorkspaceSchema),
         sessionMiddleware,
         async (c) => {
             const databases = c.get("databases");
+            const storage = c.get("storage");
             const user = c.get("user");
 
-            const { name } = c.req.valid("json")
+            const { name, image } = c.req.valid("form");
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                );
+
+                /* 
+                arrayBuffer는 무료 티어에서 사용할 수 없음.
+                따라서 기존 이미지 그대로 출력 되게 변경
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id,
+                );
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`; 
+                */
+
+                uploadedImageUrl = `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${IMAGES_BUCKET_ID}/files/${file.$id}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
+
+            }
 
             const workspace = await databases.createDocument(
                 DATABASE_ID,
@@ -25,7 +50,8 @@ const app = new Hono()
                 {
                     name,
                     userId: user.$id,
-                }
+                    imageUrl: uploadedImageUrl,
+                },
             );
 
             return c.json({ data: workspace });
