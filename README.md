7c8f65515d0742b08a0e7d80decb0110cffab8aa
ZeTta062 <samhak17@naver.com>
2025-08-19 10:22:31 +0900

    Build a workspace switcher

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
 - npm i @hono/zod-validator
 - npm i node-appwrite@latest
 - npm i server-only
+
+## 수정해야할 점
+- appwrite는 Image transformations를 free tier에서 제공하지 않음.
+따라서 getFilePreview를 사용할 수 없음
++ 우회방법으로 base64프리뷰를 사용하지 않고, 이미지 자체를 가져옴.
++ storage 설정에서 Permissons에 Read Any 설정함.
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 
 import { Navigation } from "./navigation"
 import { DottedSeparator } from "./dotted-separator"
+import WorkspaceSwitcher from "./workspace-switcher"
 
 export const Sidebar = () => {
     return (
@@ -10,6 +11,8 @@ export const Sidebar = () => {
             <Link href={"/"}>
                 <Image src={"/logo.svg"} alt="로고" width={164} height={48} />
             </Link>
+            <DottedSeparator className="my-4" />
+            <WorkspaceSwitcher />
             <DottedSeparator className="my-4" />
             <Navigation />
         </aside>
