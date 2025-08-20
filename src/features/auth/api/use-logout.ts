30b537cce5e9241e9cd02691727619e5a951babc
ZeTta062 <samhak17@naver.com>
2025-08-20 11:26:22 +0900

    Create a workspace member

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -30,6 +30,7 @@ export const useLogout = () => {
             toast.success("로그아웃에 성공하였습니다.")
             router.refresh();
             queryClient.invalidateQueries({ queryKey: ["current"] });
+            queryClient.invalidateQueries({ queryKey: ["workspaces"] });
         },
         onError: () => {
             toast.error("로그아웃에 실패하였습니다.")
